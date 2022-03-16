d9f750df91e9394bbe3414785455b5c00f4de66c
EC2 Default User <ec2-user@ip-172-31-94-2.ec2.internal>
2022-03-16 03:10:00 +0000

    first commit

--- janken.rb
+++ janken.rb
@@ -13,10 +13,10 @@ class Janken
   
   def type
     @options = {
-    0 => 'グー',
-    1 => 'チョキ',
-    2 => 'パー',
-    3 => '戦わない'
+      0 => "グー",
+      1 => "チョキ",
+      2 => "パー",
+      3 => "戦わない"
     }
     @options.each { |key, name| puts "#{key}(#{name})" }
   end
@@ -25,10 +25,10 @@ class Janken
     @player_hand = @options[gets.to_i]
     @program_hand = @options[rand(4)]
     puts "ホイ！"
-    puts '------------'
+    puts "------------"
     puts "あなた:#{@player_hand}を出しました"
     puts "相手:#{@program_hand}を出しました"
-    puts '------------'
+    puts "------------"
     result
   end
   
@@ -36,11 +36,13 @@ class Janken
     if @player_hand == @program_hand
       puts "あいこで"
       play
-    elsif (@player_hand == 0 && @program_hand == 1)||(@player_hand == 1 && @program_hand == 2)||(@player_hand == 2 && @program_hand == 0)
+    elsif (@player_hand == "グー" && @program_hand == "チョキ")||(@player_hand == "チョキ" && @program_hand == "パー")||(@player_hand == "パー" && @program_hand == "グー")
       puts "あなたの勝ちです"
-    elsif (@player_hand == 0 && @program_hand == 2)||(@player_hand == 1 && @program_hand == 0)||(@player_hand == 2 && @program_hand == 1)
+      puts "------------"
+    elsif (@player_hand == "グー" && @program_hand == "パー")||(@player_hand == "チョキ" && @program_hand == "グー")||(@player_hand == "パー" && @program_hand == "チョキ")
       puts "相手の勝ちです"
-    elsif (@player_hand == 3 && @program_hand == 0)||(@player_hand == 3 && @program_hand == 1)||(@player_hand == 3 && @program_hand == 2)||(@player_hand == 0 && @program_hand == 3)||(@player_hand == 1 && @program_hand == 3)||(@player_hand == 2 && @program_hand == 3)
+      puts "------------"
+    elsif (@player_hand == "戦わない")||(@program_hand == "戦わない")
       start
     end
   end
@@ -48,10 +50,10 @@ class Janken
   def direction
     puts "あっちむいて"
     @directions = {
-      0 => '上',
-      1 => '下',
-      2 => '左',
-      3 => '右'
+      0 => "上",
+      1 => "下",
+      2 => "左",
+      3 => "右"
     }
     @directions.each { |key, name| puts "#{key}(#{name})" }
   end
@@ -60,16 +62,17 @@ class Janken
     @player_finger = @directions[gets.to_i]
     @program_finger = @directions[rand(4)]
     puts "ほい"
-    puts '-----------'
+    puts "------------"
     puts "あなた:#{@player_finger}"
     puts "相手:#{@program_finger}"
-    puts '-----------'
+    puts "------------"
     game
   end
   
   def game
     if @player_finger == @program_finger
       puts "勝利!"
+      puts "------------"
       start
     else 
       start
